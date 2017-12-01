5ab017ef8a85aa7bfbddf32e8b03c281368998d2
Ramon <pcnay2003@gmail.com>
2017-11-30 18:45:25 -0800

    Modificacion 1

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+    // Probando el github
+    // Las constantes en PHP se definen con mayusculas.
+    
+    define("SERVER","localhost");
+    define("USER","root");
+    define("PASS","");
+    define("BD","super_heroes");
+?>
